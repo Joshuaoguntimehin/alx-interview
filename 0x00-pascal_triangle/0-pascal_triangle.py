ab0c6bfe3999b8122b007ecd3eceb04aa0c86fbc
joshua oguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-03 16:33:16 -0700

    new

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,14 @@
-function pascalTriangle(n) {
-    if (n <= 0) {
-        return [];
-    }
-    
-    const triangle = [];
-    
-    for (let i = 0; i < n; i++) {
-        const row = new Array(i + 1).fill(1); // Start with a row of 1s
-        
-        for (let j = 1; j < i; j++) {
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]; // Sum of two elements above
-        }
-        
-        triangle.push(row);
-    }
-    
-    return triangle;
-}
+#!/bin/usr/bash
+def pascal_triangle(n):
+    if n <= 0:
+        return []
 
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)  # Start with a row of 1s
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]  # Sum of two elements above
+        triangle.append(row)
+
+    return triangle

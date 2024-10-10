4bb6dfe5e6d39ceaba5469d9b10ca4bded06b003
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-10 18:24:26 +0100

    alx tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,12 +5,12 @@ def canUnlockAll(boxes):
     unlocked[0] = True      # Box 0 is already unlocked
     keys = boxes[0]         # Start with the keys in box 0
     stack = [0]             # Stack to simulate unlocking boxes
-    
+
     while stack:
         box = stack.pop()
         for key in boxes[box]:
             if key < n and not unlocked[key]:
                 unlocked[key] = True
                 stack.append(key)
-    
+
     return all(unlocked)

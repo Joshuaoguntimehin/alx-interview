465afe1b3439df206cd2a9ef217d980944014f90
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-10 18:28:54 +0100

    alx tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
     """Track if each box is unlocked"""
-    n = len(boxes)
-    unlocked = [False] * n  # Track which boxes are unlocked
-    unlocked[0] = True      # Box 0 is already unlocked
-    keys = boxes[0]         # Start with the keys in box 0
-    stack = [0]             # Stack to simulate unlocking boxes
-    """The first box is unlocked"""
-    while stack:
-        box = stack.pop()
-        for key in boxes[box]:
-            if key < n and not unlocked[key]:
-                unlocked[key] = True
-                stack.append(key)
+    n = len(boxes)  # Total number of boxes
 
+    # List to track whether each box is unlocked; initially, only box 0 is unlocked
+    unlocked = [False] * n  
+    unlocked[0] = True  # The first box (box 0) is always unlocked
+
+    # Get the keys from the first box (box 0) to start unlocking other boxes
+    keys = boxes[0]
+
+    # Stack to track the boxes to be processed; start with box 0
+    stack = [0]
+
+    # Process each box in the stack until there are no more boxes to process
+    while stack:
+        # Pop the last box added to the stack (LIFO behavior)
+        box = stack.pop()
+
+        # Iterate through each key in the current box to unlock more boxes
+        for key in boxes[box]:
+            # If the key is valid (within the range of available boxes) and the box is still locked
+            if key < n and not unlocked[key]:
+                unlocked[key] = True  # Mark the box as unlocked
+                stack.append(key)     # Add this newly unlocked box to the stack to process its keys later
+
+    # If all boxes are unlocked, return True, otherwise return False
     return all(unlocked)

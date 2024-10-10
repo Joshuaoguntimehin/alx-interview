86aa8af1e634ffd5f01da9e8b0b6a276285e131c
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-10 18:26:55 +0100

    alx tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
+    """Track if each box is unlocked"""
     n = len(boxes)
     unlocked = [False] * n  # Track which boxes are unlocked
     unlocked[0] = True      # Box 0 is already unlocked
     keys = boxes[0]         # Start with the keys in box 0
     stack = [0]             # Stack to simulate unlocking boxes
-
+    """The first box is unlocked"""
     while stack:
         box = stack.pop()
         for key in boxes[box]:

9f38129a2b23b447146d2c8c2a61283955ba7d55
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-24 19:12:31 +0100

    alx tasks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
-
-# Initialize total file size and status code counts
+"""Initialize total file size and status code counts"""
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0  # To track every 10 lines

931bb5ee5a3043587dd74523d8675c73f9d887b0
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-10-24 18:38:52 +0100

    alx tasks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,10 +2,12 @@
 """import statement"""
 import sys
 
-total_size = 0 
+
+total_size = 0
 status_counts = {}
-valid_codes =  200, 301, 400, 401, 403, 404, 405, 500
+valid_codes = 200, 301, 400, 401, 403, 404, 405, 500
 line_count = 0
+
 
 def print_stats():
     """Prints the total size and the status code counts."""
@@ -13,21 +15,22 @@ def print_stats():
     for code in sorted(status_counts):
         if status_counts[code] > 0:
             print(f"{code}: {status_counts[code]}")
-            
+
+
 try:
     for line in sys.stdin:
         parts = line.split()
-        if len(parts) <7:
+        if len(parts) < 7:
             continue
-        
+
         try:
             status_code = int(parts[-2])
             file_size = int(parts[-1])
         except ValueError:
             continue
-        
-        total_size +=file_size
-        
+
+        total_size += file_size
+
         if status_code in valid_codes:
             status_counts[status_code] = status_counts.get(status_code, 0) + 1
         line_count += 1
@@ -37,5 +40,3 @@ try:
 except KeyboardInterrupt:
     print_stats()
     sys.exit(0)
-            
-        

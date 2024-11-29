94335effbdc0bf452a5ffe6842bc5890d9cdf853
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-29 02:10:37 +0100

    alx tasks

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,14 +13,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     
-    # Initialize an array to store the minimum coins needed for each value up to total
+    ''' Initialize an array to store the minimum coins needed for each value up to total'''
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # Base case: 0 coins are needed to make 0 total
     
-    # Iterate through all coin denominations
+    '''Iterate through all coin denominations'''
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
     
-    # If dp[total] is still float('inf'), it means the total cannot be met
+    ''' If dp[total] is still float('inf'), it means the total cannot be met'''
     return dp[total] if dp[total] != float('inf') else -1

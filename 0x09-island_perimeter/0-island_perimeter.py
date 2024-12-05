f8fd216f6c38cd6fee2f8ddc0c63ea7d402a9001
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-12-06 00:18:43 +0100

    alx tasks

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""import statement"""
+def island_perimeter(grid):
+    """
+    Args:
+        grid (_type_): _description_
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols= len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter +=1
+
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][i - 1] == 0:
+                    perimeter +=1
+                if j == rows - 1 or grid[i][i + 1] == 0:
+                    perimeter += 1
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,6 @@
+Compressing objects: 100% (7/7), done.
+Writing objects: 100% (7/7), 1.24 KiB | 637.00 KiB/s, done.
+Total 7 (delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/Joshuaoguntimehin/alx-interview.git
+   8e4adf4..7e78098  main -> main
